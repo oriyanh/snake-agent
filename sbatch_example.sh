8d81ee3848a0611e68138fcc0f280086ae1f893a
Oriyan Hermoni <oriyanh@cs.huji.ac.il>
2020-02-25 15:00:12 +0200

    Updated batch file

--- sbatch_example.sh
+++ sbatch_example.sh
@@ -1,7 +1,7 @@
 #!/bin/csh
 
 #SBATCH --cpus-per-task=2
-#SBATCH --output=<YOUR_OUTPUT_PATH_HERE>
+#SBATCH --output=~/Courses/APML/snake-apml/out
 #SBATCH --mem-per-cpu=500M
 #SBATCH --account=aml
 #SBATCH --constraint="sm"
@@ -9,4 +9,4 @@
 source /cs/labs/dshahaf/omribloch/env/snake/snake/bin/activate.csh
 module load tensorflow
 
-python3 <YOUR_FOLDER>/Snake.py -P "Avoid(epsilon=0.5);Avoid(epsilon=0.2);MyPolicy(lr=0.001);MyPolicy(lr=0.001)" -D 5000 -s 1000 -l "<YOUR_LOG_PATH>" -r 0 -plt 0.01 -pat 0.005 -pit 60
+python3 ~/Courses/APML/snake-apml/Snake.py -P "Linear()" -D 5000 -s 1000 -l "~/Courses/APML/snake-apml/out/game_linear.log" -o "~/Courses/APML/snake-apml/out/game_linear.out" -r 0 -plt 0.01 -pat 0.005 -pit 20
